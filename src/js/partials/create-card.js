84531006bb84d5bb3b799baae9b3d59d3eb5a5fb
WhiteN <whiten.ncs@gmail.com>
2019-08-20 20:10:34 +0300

    create event open/close modal

--- src/js/partials/create-card.js
+++ src/js/partials/create-card.js
@@ -39,4 +39,40 @@
         }
     }
 
+    // Получение списка докторов с их полями с БД
+    const doctors = getDoctors();
+    
+    const createVisit = document.getElementById('header-btn'),
+          modalCard = document.getElementById('modal-card'),
+          modalCardClose = document.getElementById('modal-card-close'),
+          selectDoctor = document.getElementById('select-doctor'),
+          inputComment = document.getElementById('input-comment'),
+          inputCommon= document.querySelector('.input-box__common'),
+          inputDate = document.getElementById('input-date'),
+          submitModal = document.getElementById('submit-btn__modal'),
+          cardsContainer = document.getElementById('cards-container');
+
+    // Открытые модалки
+    createVisit.addEventListener('click', function(e) {
+        e.stopPropagation();
+        modalCard.classList.remove('modal-card--hide');
+    })
+
+    // Закрытие модалки по кнопке
+    modalCardClose.addEventListener('click', function(e) {
+        e.stopPropagation();
+        modalCard.classList.add('modal-card--hide');
+    })
+
+    // Закрытие модалки по клику в не окна
+    document.body.addEventListener('click', function(e) {
+        const modalCardHide = document.querySelector('.modal-card--hide');
+        
+        if (modalCardHide === null) {
+            if((findAncestor(e.target, 'modal-card')) === null) {
+                modalCard.classList.add('modal-card--hide');
+            }
+        }
+    })
+
 })();
